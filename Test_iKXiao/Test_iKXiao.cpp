5485b1292f2e4ddf81966e4dbb4f62658b913271
mensong <coolcn@qq.com>
2023-11-13 18:34:20 +0800

    改为对象类型API

--- Test_iKXiao/Test_iKXiao.cpp
+++ Test_iKXiao/Test_iKXiao.cpp
@@ -4,36 +4,140 @@
 #include <iostream>
 #include "..\iKXiao\iKXiao.h"
 
+void normalTest()
+{
+    WorkBook* wb = iKXiao::Ins().OpenExcel("admin.xlsx", NULL);
+    if (wb)
+    {
+        WorkSheet* sheet = wb->OpenCurrentSheet();
+        if (sheet)
+        {
+            size_t rowCount = sheet->GetRowCount();
+            for (size_t r = 0; r < rowCount; r++)
+            {
+                size_t columnCount = 0;
+                char* rowData = sheet->GetRowStringArray(r, &columnCount);
+                size_t offset = 0;
+                for (size_t c = 0; c < columnCount; c++)
+                {
+                    std::cout << rowData + offset << ",";
+                    offset += strlen(rowData + offset) + 1;
+                }
+                sheet->FreeString(rowData);
+                std::cout << std::endl;
+            }
+
+            Cell* cell = sheet->OpenCell(0, 0);
+            if (cell)
+            {
+                cell->SetStringValue("我爱你mensong");
+                std::string cellstr = cell->GetStringValue("");
+                std::cout << "(0,0) = " << cellstr << std::endl;
+                sheet->CloseCell(cell);
+            }
+
+
+            cell = sheet->OpenCell("A1");
+            if (cell)
+            {
+                std::string cellstr = cell->GetStringValue("");
+                std::cout << "A1 = " << cellstr << std::endl;
+                sheet->CloseCell(cell);
+            }
+
+            cell = sheet->OpenCell("A2");
+            if (cell)
+            {
+                cell->SetFormula("=SUM(1,2,3)");
+                int intVal = cell->GetIntValue(0);
+                std::cout << "=SUM(1,2,3) = " << intVal << std::endl;
+                sheet->CloseCell(cell);
+            }
+
+            wb->CloseSheet(sheet);
+        }
+
+        wb->Save("admin1.xlsx");
+
+        iKXiao::Ins().CloseExcel(wb);
+    }
+}
+
+void testConfig()
+{
+    WorkBook* wb = iKXiao::Ins().OpenExcel("config.xlsx", NULL);
+    if (wb)
+    {
+        WorkSheet* sheet = wb->OpenCurrentSheet();
+        if (sheet)
+        {            
+            size_t idxRowStart = sheet->GetNotEmptyRowStart();
+            size_t idxRowEnd = sheet->GetNotEmptyRowEnd();
+            size_t idxColumnStart = sheet->GetNotEmptyColumnStart();
+            size_t idxColumnEnd = sheet->GetNotEmptyColumnEnd();
+            for (size_t r = idxRowStart; r <= idxRowEnd; r++)
+            {
+                for (size_t c = idxColumnStart; c <= idxColumnEnd; c++)
+                {
+                    Cell* cell = sheet->OpenCell(r, c);
+                    if (cell)
+                    {
+                        auto type = cell->GetValueType();
+                        switch (type)
+                        {
+                        case Cell::empty_value:
+                            std::cout << "<NULL>" << ',';
+                            break;
+                        case Cell::boolean_value:
+                            std::cout << cell->GetBoolValue() << ',';
+                            break;
+                        case Cell::date_value:
+                        {
+                            int year = 0;
+                            int month = 0;
+                            int day = 0;
+                            cell->GetDateValue(year, month, day);
+                            std::cout << year << "年" << month << "月" << day << "日" << ',';
+                            break;
+                        }
+                        case Cell::error_value:
+                            std::cout << "#VALUE!" << ',';
+                            break;
+                        case Cell::inline_string_value:
+                        case Cell::shared_string_value:
+                            std::cout << cell->GetStringValue() << ',';
+                            break;
+                        case Cell::number_value:
+                            std::cout << cell->GetDoubleValue() << ',';
+                            break;
+                        case Cell::formula_string_value:
+                            std::cout << cell->GetDoubleValue() << ',';
+                            break;
+                        default:
+                            std::cout << cell->GetStringValue() << ',';
+                            break;
+                        }
+
+                        sheet->CloseCell(cell);
+                    }
+                }
+                std::cout << std::endl;
+            }
+
+            wb->CloseSheet(sheet);
+        }
+
+        iKXiao::Ins().CloseExcel(wb);
+    }
+}
+
 int main()
 {
-    IK_WORKBOOK wb = iKXiao::Ins().OpenExcel("E:\\admin.xlsx", NULL);
+    normalTest();
 
-    size_t rowCount = iKXiao::Ins().GetRowCount(wb, IDX_SHEET_CUR);
-    for (size_t r = 0; r < rowCount; r++)
-    {
-        size_t columnCount = 0;
-        char* rowData = iKXiao::Ins().GetRowStringArray(wb, IDX_SHEET_CUR, r, &columnCount);
-        size_t offset = 0;
-        for (size_t c = 0; c < columnCount; c++)
-        {
-            std::cout << rowData + offset << ",";
-            offset += strlen(rowData + offset) + 1;
-        }
-        iKXiao::Ins().FreeString(wb, rowData);
-        std::cout << std::endl;
-    }
+    std::cout << std::endl << std::endl;
     
+    testConfig();
 
-    char* cellStr = iKXiao::Ins().GetCellStringValue(wb, IDX_SHEET_CUR, 0, 0);
-    char* cellStr1 = iKXiao::Ins().GetCellStringValueByRefName(wb, IDX_SHEET_CUR, "A1");
-
-    iKXiao::Ins().SetCellStringValue(wb, IDX_SHEET_CUR, 0, 0, "我爱你mensong");
-    iKXiao::Ins().SetCellFormula(wb, IDX_SHEET_CUR, 0, 1, "=SUM(1,2,3)");
-    auto type = iKXiao::Ins().GetCellType(wb, IDX_SHEET_CUR, 0, 1);
-    char* formulaValue = iKXiao::Ins().GetCellStringValue(wb, IDX_SHEET_CUR, 0, 1);
-
-    iKXiao::Ins().SaveExcel(wb, "E:\\admin1.xlsx", NULL);
-
-    iKXiao::Ins().CloseExcel(wb);
     return 0;
 }
--- iKXiao/iKXiao.h
+++ iKXiao/iKXiao.h
@@ -11,74 +11,105 @@
 #define IKXIAO_API extern "C" __declspec(dllimport)
 #endif
 
-typedef void* IK_WORKBOOK;
-//typedef void* IK_WORKSHEET;
 
-#define IDX_SHEET_CUR -1
-#define IDX_SHEET_LAST -2
 
-enum CellType
+class Cell
 {
-	/// no value
-	empty_value,
-	/// value is TRUE or FALSE
-	boolean_value,
-	/// value is an ISO 8601 formatted date
-	date_value,
-	/// value is a known error code such as \#VALUE!
-	error_value,
-	/// value is a string stored in the cell
-	inline_string_value,
-	/// value is a number
-	number_value,
-	/// value is a string shared with other cells to save space
-	shared_string_value,
-	/// value is the string result of a formula
-	formula_string_value
+public:
+	enum CellType
+	{
+		/// no value
+		empty_value,
+		/// value is TRUE or FALSE
+		boolean_value,
+		/// value is an ISO 8601 formatted date
+		date_value,
+		/// value is a known error code such as \#VALUE!
+		error_value,
+		/// value is a string stored in the cell
+		inline_string_value,
+		/// value is a number
+		number_value,
+		/// value is a string shared with other cells to save space
+		shared_string_value,
+		/// value is the string result of a formula
+		formula_string_value
+	};
+
+public:
+	virtual CellType		GetValueType() = 0;
+	virtual char*			GetStringValue(const char* defVal = "") = 0;
+	virtual bool			SetStringValue(const char* strVal) = 0;
+	virtual bool			SetNullValue() = 0;
+	virtual bool			SetBoolValue(bool boolean_value) = 0;
+	virtual bool			GetBoolValue(bool defVal = false) = 0;
+	virtual bool			SetIntValue(int int_value) = 0;
+	virtual int				GetIntValue(int defVal = 0) = 0;
+	virtual bool			SetUIntValue(unsigned int int_value) = 0;
+	virtual unsigned int	GetUIntValue(unsigned int defVal = 0) = 0;
+	virtual bool			SetLLIntValue(long long int int_value) = 0;
+	virtual long long int	GetLLIntValue(long long int defVal = 0) = 0;
+	virtual bool			SetULLIntValue(unsigned long long int int_value) = 0;
+	virtual unsigned long long int GetULLIntValue(unsigned long long int defVal = 0) = 0;
+	virtual bool			SetDoubleValue(double float_value) = 0;
+	virtual double			GetDoubleValue(double defVal = 0.0) = 0;
+	virtual bool			SeDateValue(int year_, int month_, int day_) = 0;
+	virtual bool			GetDateValue(int& year_, int& month_, int& day_) = 0;
+	virtual bool			SetDatetimeValue(int year_, int month_, int day_, 
+												 int hour_ = 0, int minute_ = 0, int second_ = 0, int microsecond_ = 0) = 0;
+	virtual bool			GetDatetimeValue(int& year_, int& month_, int& day_, 
+												 int& hour_, int& minute_, int& second_, int& microsecond_) = 0;
+	virtual bool			SetTimeValue(int hour_ = 0, int minute_ = 0, int second_ = 0, int microsecond_ = 0) = 0;
+	virtual bool			GetTimeValue(int& hour_, int& minute_, int& second_, int& microsecond_) = 0;
+	virtual bool			SetTimeDeltaValue(int days_, int hours_, int minutes_, int seconds_, int microseconds_) = 0;
+	virtual bool			GetTimeDeltaValue(int days_, int& hours_, int& minutes_, int& seconds_, int& microseconds_) = 0;
+	virtual bool			SetFormula(const char* formula) = 0;
+	virtual bool			CopyFrom(Cell* otherCell) = 0;
+
+	virtual void			FreeString(char* buff) = 0;
 };
 
-IKXIAO_API IK_WORKBOOK	OpenExcel(const char* xlsxFilepath, const char* password);
-IKXIAO_API int			GetSheetCount(IK_WORKBOOK workbook);
-IKXIAO_API int			GetSheetIndexByTitle(IK_WORKBOOK workbook, const char* sheetTitle);
-IKXIAO_API char*		GetSheetTitle(IK_WORKBOOK workbook, int sheetIndex);
-IKXIAO_API bool			SetCurrentSheet(IK_WORKBOOK workbook, int sheetIndex);
-IKXIAO_API int			CreateSheet(IK_WORKBOOK workbook, int atIndex = -1);
-IKXIAO_API int			CopySheet(IK_WORKBOOK workbook, int srcIndex, int atIndex = -1);
-IKXIAO_API bool			RemoveSheet(IK_WORKBOOK workbook, int sheetIndex);
-IKXIAO_API bool			SetSheetTitle(IK_WORKBOOK workbook, int sheetIndex, const char* title);
-IKXIAO_API size_t		GetRowCount(IK_WORKBOOK workbook, int sheetIndex);
-//获得行数据，数据格式为"列1内容\0列2内容\0列3内容\0"
-IKXIAO_API char*		GetRowStringArray(IK_WORKBOOK workbook, int sheetIndex, size_t rowIndex, size_t* columnCount);
-IKXIAO_API CellType		GetCellType(IK_WORKBOOK workbook, int sheetIndex, size_t rowIndex, size_t columnIndex);
-IKXIAO_API char*		GetCellStringValue(IK_WORKBOOK workbook, int sheetIndex, size_t rowIndex, size_t columnIndex);
-IKXIAO_API char*		GetCellStringValueByRefName(IK_WORKBOOK workbook, int sheetIndex, const char* refName);
-IKXIAO_API bool			SetCellStringValue(IK_WORKBOOK workbook, int sheetIndex, size_t rowIndex, size_t columnIndex, const char* strVal);
-IKXIAO_API bool			SetCellNullValue(IK_WORKBOOK workbook, int sheetIndex, size_t rowIndex, size_t columnIndex);
-IKXIAO_API bool			SetCellBoolValue(IK_WORKBOOK workbook, int sheetIndex, size_t rowIndex, size_t columnIndex, bool boolean_value);
-IKXIAO_API bool			GetCellBoolValue(IK_WORKBOOK workbook, int sheetIndex, size_t rowIndex, size_t columnIndex, bool defVal = false);
-IKXIAO_API bool			SetCellIntValue(IK_WORKBOOK workbook, int sheetIndex, size_t rowIndex, size_t columnIndex, int int_value);
-IKXIAO_API int			GetCellIntValue(IK_WORKBOOK workbook, int sheetIndex, size_t rowIndex, size_t columnIndex, int defVal = 0);
-IKXIAO_API bool			SetCellUIntValue(IK_WORKBOOK workbook, int sheetIndex, size_t rowIndex, size_t columnIndex, unsigned int int_value);
-IKXIAO_API unsigned int GetCellUIntValue(IK_WORKBOOK workbook, int sheetIndex, size_t rowIndex, size_t columnIndex, unsigned int defVal = 0);
-IKXIAO_API bool			SetCellLLIntValue(IK_WORKBOOK workbook, int sheetIndex, size_t rowIndex, size_t columnIndex, long long int int_value);
-IKXIAO_API long long int GetCellLLIntValue(IK_WORKBOOK workbook, int sheetIndex, size_t rowIndex, size_t columnIndex, long long int defVal = 0);
-IKXIAO_API bool			SetCellULLIntValue(IK_WORKBOOK workbook, int sheetIndex, size_t rowIndex, size_t columnIndex, unsigned long long int int_value);
-IKXIAO_API unsigned long long int GetCellULLIntValue(IK_WORKBOOK workbook, int sheetIndex, size_t rowIndex, size_t columnIndex, unsigned long long int defVal = 0);
-IKXIAO_API bool			SetCellDoubleValue(IK_WORKBOOK workbook, int sheetIndex, size_t rowIndex, size_t columnIndex, double float_value);
-IKXIAO_API double		GetCellDoubleValue(IK_WORKBOOK workbook, int sheetIndex, size_t rowIndex, size_t columnIndex, double defVal = 0.0);
-IKXIAO_API bool			SetCellDateValue(IK_WORKBOOK workbook, int sheetIndex, size_t rowIndex, size_t columnIndex, int year_, int month_, int day_);
-IKXIAO_API bool			GetCellDateValue(IK_WORKBOOK workbook, int sheetIndex, size_t rowIndex, size_t columnIndex, int& year_, int& month_, int& day_);
-IKXIAO_API bool			SetCellDatetimeValue(IK_WORKBOOK workbook, int sheetIndex, size_t rowIndex, size_t columnIndex, int year_, int month_, int day_, int hour_ = 0, int minute_ = 0, int second_ = 0, int microsecond_ = 0);
-IKXIAO_API bool			GetCellDatetimeValue(IK_WORKBOOK workbook, int sheetIndex, size_t rowIndex, size_t columnIndex, int& year_, int& month_, int& day_, int& hour_, int& minute_, int& second_, int& microsecond_);
-IKXIAO_API bool			SetCellTimeValue(IK_WORKBOOK workbook, int sheetIndex, size_t rowIndex, size_t columnIndex, int hour_ = 0, int minute_ = 0, int second_ = 0, int microsecond_ = 0);
-IKXIAO_API bool			GetCellTimeValue(IK_WORKBOOK workbook, int sheetIndex, size_t rowIndex, size_t columnIndex, int& hour_, int& minute_, int& second_, int& microsecond_);
-IKXIAO_API bool			SetCellTimeDeltaValue(IK_WORKBOOK workbook, int sheetIndex, size_t rowIndex, size_t columnIndex, int days_, int hours_, int minutes_, int seconds_, int microseconds_);
-IKXIAO_API bool			GetCellTimeDeltaValue(IK_WORKBOOK workbook, int sheetIndex, size_t rowIndex, size_t columnIndex, int days_, int& hours_, int& minutes_, int& seconds_, int& microseconds_);
-IKXIAO_API bool			SetCellFormula(IK_WORKBOOK workbook, int sheetIndex, size_t rowIndex, size_t columnIndex, const char* formula);
-IKXIAO_API bool			CopyCell(IK_WORKBOOK workbook, int sheetIndex, size_t fromRowIndex, size_t fromColumnIndex, size_t toRowIndex, size_t toColumnIndex);
-IKXIAO_API bool			SaveExcel(IK_WORKBOOK workbook, const char* filepath, const char* password);
-IKXIAO_API void			FreeString(IK_WORKBOOK workbook, char* buff);
-IKXIAO_API void			CloseExcel(IK_WORKBOOK workbook);
+class WorkSheet
+{
+public:
+	virtual char*			GetSheetTitle() = 0;
+	virtual bool			SetSheetTitle(const char* title) = 0;
+	virtual size_t			GetRowCount() = 0;
+	virtual size_t			GetColumnCount() = 0;
+	virtual size_t			GetNotEmptyRowStart() = 0;
+	virtual size_t			GetNotEmptyRowEnd() = 0;
+	virtual size_t			GetNotEmptyColumnStart() = 0;
+	virtual size_t			GetNotEmptyColumnEnd() = 0;
+	//获得行数据，数据格式为"列1内容\0列2内容\0列3内容\0"
+	virtual char*			GetRowStringArray(size_t rowIndex, size_t* columnCount) = 0;
+
+	virtual Cell*			OpenCell(size_t rowIdx, int columnIdx) = 0;
+	virtual Cell*			OpenCell(const char* refName) = 0;
+	virtual void			CloseCell(Cell* cell) = 0;
+
+	virtual void			FreeString(char* buff) = 0;
+};
+
+class WorkBook
+{
+public:
+	virtual int				GetSheetCount() = 0;
+	virtual WorkSheet*		OpenSheetByIndex(int sheetIndex) = 0;
+	virtual WorkSheet*		OpenSheetByTitle(const char* sheetTitle) = 0;
+	virtual bool			SetCurrentSheet(WorkSheet* sheet) = 0;
+	virtual WorkSheet*		OpenCurrentSheet() = 0;
+	virtual WorkSheet*		CreateSheet(int atIndex = -1) = 0;
+	virtual WorkSheet*		CloneSheet(WorkSheet* srcSheet, int atIndex = -1) = 0;
+	virtual bool			RemoveSheet(WorkSheet* sheet) = 0;
+	virtual bool			Save(const char* filepath) = 0;
+
+	virtual void			CloseSheet(WorkSheet* sheet) = 0;
+
+	virtual void			FreeString(char* buff) = 0;	
+};
+
+IKXIAO_API WorkBook*		OpenExcel(const char* xlsxFilepath, const char* password);
+IKXIAO_API void				CloseExcel(WorkBook* workbook);
 
 class iKXiao
 {
@@ -96,89 +127,11 @@ public:
 			return;
 
 		SET_PROC(hDll, OpenExcel);
-		SET_PROC(hDll, GetSheetCount);
-		SET_PROC(hDll, GetSheetIndexByTitle);
-		SET_PROC(hDll, GetSheetTitle);
-		SET_PROC(hDll, SetCurrentSheet);
-		SET_PROC(hDll, CreateSheet);
-		SET_PROC(hDll, CopySheet);
-		SET_PROC(hDll, RemoveSheet);
-		SET_PROC(hDll, SetSheetTitle);
-		SET_PROC(hDll, GetRowCount);
-		SET_PROC(hDll, GetRowStringArray);
-		SET_PROC(hDll, GetCellType);
-		SET_PROC(hDll, GetCellStringValue);
-		SET_PROC(hDll, GetCellStringValueByRefName);
-		SET_PROC(hDll, SetCellStringValue);
-		SET_PROC(hDll, SetCellNullValue);
-		SET_PROC(hDll, SetCellBoolValue);
-		SET_PROC(hDll, GetCellBoolValue);
-		SET_PROC(hDll, SetCellIntValue);
-		SET_PROC(hDll, GetCellIntValue);
-		SET_PROC(hDll, SetCellUIntValue);
-		SET_PROC(hDll, GetCellUIntValue);
-		SET_PROC(hDll, SetCellLLIntValue);
-		SET_PROC(hDll, GetCellLLIntValue);
-		SET_PROC(hDll, SetCellULLIntValue);
-		SET_PROC(hDll, GetCellULLIntValue);
-		SET_PROC(hDll, SetCellDoubleValue);
-		SET_PROC(hDll, GetCellDoubleValue);
-		SET_PROC(hDll, SetCellDateValue);
-		SET_PROC(hDll, GetCellDateValue);
-		SET_PROC(hDll, SetCellDatetimeValue);
-		SET_PROC(hDll, GetCellDatetimeValue);
-		SET_PROC(hDll, SetCellTimeValue);
-		SET_PROC(hDll, GetCellTimeValue);
-		SET_PROC(hDll, SetCellTimeDeltaValue);
-		SET_PROC(hDll, GetCellTimeDeltaValue);
-		SET_PROC(hDll, SetCellFormula);
-		SET_PROC(hDll, CopyCell);
-		SET_PROC(hDll, SaveExcel);
-		SET_PROC(hDll, FreeString);
 		SET_PROC(hDll, CloseExcel);
 	}
 
 
 	DEF_PROC(OpenExcel);
-	DEF_PROC(GetSheetCount);
-	DEF_PROC(GetSheetIndexByTitle);
-	DEF_PROC(GetSheetTitle);
-	DEF_PROC(SetCurrentSheet);
-	DEF_PROC(CreateSheet);
-	DEF_PROC(CopySheet);
-	DEF_PROC(RemoveSheet);
-	DEF_PROC(SetSheetTitle);
-	DEF_PROC(GetRowCount);
-	DEF_PROC(GetRowStringArray);
-	DEF_PROC(GetCellType);
-	DEF_PROC(GetCellStringValue); 
-	DEF_PROC(GetCellStringValueByRefName);
-	DEF_PROC(SetCellStringValue);
-	DEF_PROC(SetCellNullValue);
-	DEF_PROC(SetCellBoolValue);
-	DEF_PROC(GetCellBoolValue);
-	DEF_PROC(SetCellIntValue);
-	DEF_PROC(GetCellIntValue);
-	DEF_PROC(SetCellUIntValue);
-	DEF_PROC(GetCellUIntValue);
-	DEF_PROC(SetCellLLIntValue);
-	DEF_PROC(GetCellLLIntValue);
-	DEF_PROC(SetCellULLIntValue);
-	DEF_PROC(GetCellULLIntValue);
-	DEF_PROC(SetCellDoubleValue);
-	DEF_PROC(GetCellDoubleValue);
-	DEF_PROC(SetCellDateValue);
-	DEF_PROC(GetCellDateValue);
-	DEF_PROC(SetCellDatetimeValue);
-	DEF_PROC(GetCellDatetimeValue);
-	DEF_PROC(SetCellTimeValue);
-	DEF_PROC(GetCellTimeValue);
-	DEF_PROC(SetCellTimeDeltaValue);
-	DEF_PROC(GetCellTimeDeltaValue);
-	DEF_PROC(SetCellFormula);
-	DEF_PROC(CopyCell);
-	DEF_PROC(SaveExcel);
-	DEF_PROC(FreeString);
 	DEF_PROC(CloseExcel);
 
 
